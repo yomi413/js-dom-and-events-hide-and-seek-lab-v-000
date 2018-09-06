2d4fef6ebe655d96134ba570fb0e65f102bc06ee
Yomaira Escano <yjescano@gmail.com>
2018-09-06 20:45:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function increaseRankBy(n) {
 
 function deepestChild() {
   let children = document.getElementById('grand-node').querySelector('div')
-  let list;
+  let list
 
   for (let i = 0; i < children.length-1; i++) {
     list = children[i].querySelector('div')
