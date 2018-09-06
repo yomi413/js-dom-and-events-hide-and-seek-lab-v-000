585d7bffb59d8be9be3e0bcf295e81ba9c8697c5
Yomaira Escano <yjescano@gmail.com>
2018-09-06 20:16:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function increaseRankBy(n) {
   let list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
 
   for (let i = 0; i < list.length; i++) {
-    return list[i].innerHTML = parseInt(list[i].innerHTML) + 1
+    return list[i].innerHTML = parseInt(list[i].innerHTML) + n
   }
 }
